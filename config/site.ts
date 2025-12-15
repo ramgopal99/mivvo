4368fe2393ca3ae3f948f406b706920aedbd371a
ramgopal99 <ramgopalbagh009@gmail.com>
2025-12-15 23:59:20 +0530

    Update site configuration to disable course and foreign language features
    - Set enableCourses and enableForeignLanguage to false in site configuration, reflecting a shift in feature availability.

--- config/site.ts
+++ config/site.ts
@@ -4,8 +4,8 @@ export const siteConfig = {
   url: "https://mivvo.live",
   logo: "/mivvo.svg",
   email: "hello@mivvo.com",
-  enableCourses: true,
-  enableForeignLanguage: true,
+  enableCourses: false,
+  enableForeignLanguage: false,
   links: {
     twitter: "https://twitter.com/mivvo",
     github: "https://github.com/mivvo",
