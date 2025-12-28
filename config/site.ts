599b27367bf4ce3eeda201fad8f4ace1e790ce9d
ramgopal99 <ramgopalbagh009@gmail.com>
2025-12-29 05:21:29 +0530

    Update site configuration to disable course feature

--- config/site.ts
+++ config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig = {
   url: "https://mivvo.live",
   logo: "/mivvo.svg",
   email: "hello@mivvo.com",
-  enableCourses: true,
+  enableCourses: false,
   enableForeignLanguage: true,
   enablePricing: true,
   links: {
